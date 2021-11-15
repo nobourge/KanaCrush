69c994ba8bbeff50aa01db0015b65321f8af0fe3
nobourge <57195536+nobourge@users.noreply.github.com>
2021-11-15 17:46:25 +0100

    main
    cmakelists

---
+++ cmake-build-debug/CMakeFiles/KanaCrush.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KanaCrush.dir/main.cpp.obj"
+  "KanaCrush.exe"
+  "KanaCrush.exe.manifest"
+  "KanaCrush.lib"
+  "KanaCrush.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KanaCrush.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by bourg on 15-11-21.
+//
+/*#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/fl_draw.H>*/
+
+#include <cmath>
+#include <ctime>
+
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+int main()
+{std::cout << "txt" ;
+    return 0;
+
+}
+
