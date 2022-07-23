2535b3c874e557999c4453301de80b787a779c6f
noe <noe.bourgeois@ulb.be>
2022-07-23 15:49:54 +0200

    CMakeLists.txt :ok: CONAN_SYSREQUIRES_MODE=verify , conan install aborted

--- animation.cpp
+++ animation.cpp
@@ -1,11 +1,14 @@
 // These should include everything you might use
 #include <FL/Fl.H>
 #include <FL/fl_draw.H>
+#include <FL/Fl_Window.H>
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Box.H>
 #include <string>
-#include <math.h>
-#include <time.h>
+#include <cmath>
+//#include <math.h>
+#include <ctime>
+//#include <time.h>
 #include <chrono>
 #include <vector>
 #include <iostream>
